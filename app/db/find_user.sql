12410cbe0e666a959ffe3da7a050706da9fb4c59
Alex Ellsworth <alex.ells21@gmail.com>
2017-09-07 14:12:49 -0600

    Added more changes

--- app/db/find_user.sql
+++ app/db/find_user.sql
@@ -1,3 +1,3 @@
 SELECT *
 FROM user
-WHERE auth_id = $1;
+WHERE 'auth_id' = $1;
--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './NYG.png';
 import './App.css';
+import Login from './components/Login/Login';
+import Private from './components/Private/Private'
+
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div>
+        <Route exact path='/' component={ Login } />
+        <Route exact path='/private' component={ Private } />
+        
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          
       </div>
     );
   }
